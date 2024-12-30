cb94ce6caf6073426f4fbf52e68156661490b739
Rostislav Voloshinskiy <higasman1999@gmail.com>
2024-12-30 19:10:17 +1100

    Добавил добавление в дерево

--- AVL2.h
+++ AVL2.h
@@ -58,7 +58,80 @@ struct TreeNode {
 class AVL_tree{
 	TreeNode* root;
 	AVL_tree() : root(nullptr){}
-	void search(FIO fio, TreeNode*& p, bool& h) {
-
+	void insert(FIO fio, TreeNode*& p, bool h) {
+		TreeNode* p1 = new TreeNode(fio, 1, 0, nullptr, nullptr);
+		TreeNode* p2 = new TreeNode(fio, 1, 0, nullptr, nullptr);
+		if (p == nullptr){
+			p = new TreeNode(fio, 1, 0, nullptr, nullptr);
+			h = true;
+		}
+		else if(p->key > fio){
+			insert (fio, p->left, h);
+			if (h){
+				if(p->bal == 1){
+					p->bal = 0;
+					h= false;
+				}
+				else if (p->bal == 0){
+					p->bal = -1;
+				}
+				//значит баланс в вершине -1
+				else{
+					//может быть тут ошибка тюк я уже предопределил fio для p1
+					p1 = p->left;
+					if (p1->bal == -1){//LL rotation
+						p->left = p1->right;
+						p1->right = p;
+						p->bal = 0;
+						p = p1;
+					}
+					else{//LR rotation
+						p2 = p1->right;
+						p1->right = p2->left;
+						p2->left = p1;
+						p->left = p2->right;
+						p2->right = p;
+						if(p2->bal == -1){p->bal = 1;}
+						else{p->bal = 0;}
+						if (p2->bal == 1){p1->bal = -1;}
+						else{p1->bal = 0;}
+						p = p2;
+					}
+					p->bal = 0;
+					h = false;
+				}
+			}
+		}
+		else if(p->key < fio){
+			insert (fio, p->right, h);
+			if (h){
+				if (p->bal == -1) {p->bal=0; h=false;}
+				else if(p->bal == 0){p->bal = 1;}
+				else {
+					p1= p->right;
+					if (p1->bal == 1){//RR rotation
+						p->right = p1->left;
+						p1->left = p;
+						p->bal = 0;
+						p = p1;
+					}
+					else{//RL rotation
+						p2 = p1->left;
+						p1->left = p2->right;
+						p2->right = p1;
+						p->right = p2->left;
+						p2->left = p;
+						if (p2->bal == 1){p->bal = -1;}
+						else{p->bal = 0;}
+						if(p2->bal == -1){p1->bal = 1;}
+						else{p1->bal = 0;}
+						p = p2;
+					}
+					p->bal =0;
+					h= false;
+				}
+			}
+			else p->count++;
+		}
 	}
 }; 
